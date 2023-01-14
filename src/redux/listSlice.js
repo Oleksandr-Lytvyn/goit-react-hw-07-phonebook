fcee5fc7a306582e4f43b60ef7863918fa57e6e2
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-01-14 19:26:16 +0200

    add matchers

--- src/redux/listSlice.js
+++ src/redux/listSlice.js
@@ -1,14 +1,11 @@
 // import { nanoid } from 'nanoid';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { fetchContacts, deleteContact, addContact } from './operations';
 
+const extraActions = [fetchContacts, deleteContact, addContact];
+
 const initialState = {
-  data: [
-    // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  data: [],
   isLoading: false,
   error: null,
 };
@@ -18,43 +15,38 @@ const listSlice = createSlice({
   initialState: initialState,
   extraReducers: builder => {
     builder
-      .addCase(fetchContacts.pending, state => {
-        state.isLoading = true;
-      })
       .addCase(fetchContacts.fulfilled, (state, action) => {
         state.data = action.payload;
-        state.isLoading = false;
-      })
-      .addCase(fetchContacts.rejected, (state, action) => {
-        console.log(action.payload);
-        state.error = action.payload;
-        state.isLoading = false;
-      })
-      .addCase(deleteContact.pending, (state, action) => {
-        state.isLoading = true;
       })
       .addCase(deleteContact.fulfilled, (state, action) => {
         const index = state.data.findIndex(
           task => task.id === action.payload.id
         );
         state.data.splice(index, 1);
-        state.isLoading = false;
-      })
-      .addCase(deleteContact.rejected, (state, action) => {
-        state.error = action.payload;
-        state.isLoading = false;
-      })
-      .addCase(addContact.pending, state => {
-        state.isLoading = true;
       })
       .addCase(addContact.fulfilled, (state, action) => {
         state.data.push(action.payload);
-        state.isLoading = false;
       })
-      .addCase(addContact.rejected, (state, action) => {
-        state.error = action.payload;
-        state.isLoading = false;
-      });
+      .addMatcher(
+        isAnyOf(...extraActions.map(action => action.pending)),
+        state => {
+          state.isLoading = true;
+        }
+      )
+      .addMatcher(
+        isAnyOf(...extraActions.map(action => action.rejected)),
+        (state, action) => {
+          state.error = action.payload;
+          state.isLoading = false;
+        }
+      )
+      .addMatcher(
+        isAnyOf(...extraActions.map(action => action.fulfilled)),
+        state => {
+          state.isLoading = false;
+          state.error = null;
+        }
+      );
   },
 });
 
